08442a69df7065063c807d0dd8b474d98daf022b
Nathan Rajlich <nathan@tootallnate.net>
2016-10-04 11:57:20 -0700

    follow the debug convention of separating dirs with a colon
    Also omitting the file extension (`.js`) at the end of the
    debug name.

--- index.js
+++ index.js
@@ -16,16 +16,22 @@ function tryReadPackageJson(dir) {
 // so that next require call gets re-evaluated
 delete require.cache[__filename]
 
-dir = path.dirname(module.parent.filename))
+var filename = module.parent.filename
+if (!filename) {
+  throw new Error('`autodebug` module can not be required in the REPL!')
+}
+
+dir = path.dirname(filename)
 while (!tryReadPackageJson(dir)) {
   dir = path.dirname(dir)
 }
 
 if (!root) {
-  throw new Error('could not find package root for ' + module.parent.filename)
+  throw new Error('could not find package root for ' + filename)
 }
 
 var packageName = require(path.resolve(dir, 'package.json')).name
-var rel = path.relative(root, module.parent.filename).replace(/\\/g, '/')
+var rel = path.relative(root, filename).replace(/\.js$/, '').split(/[\/\\]/g)
+var name = packageName + ':' + rel.join(':')
 
 module.exports = debug(name)
